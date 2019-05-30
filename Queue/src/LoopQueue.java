701a2fb9633506a74d1cb444d1a5d05b9bf1ed69
YinGuiqing <CPLASFyin@163.com>
2019-05-30 18:33:24 +0800

    2019-05-30 完成循环队列的学习

--- Queue/src/LoopQueue.java
+++ Queue/src/LoopQueue.java
@@ -23,15 +23,16 @@ public class LoopQueue<T> implements Queue<T> {
 	 */
 	public LoopQueue(int capacity) {
 		data = (T[]) new Object[capacity + 1];
-		front = 0;
-		tail = 0;
-		size = 0;
+		front = 0;		//队首
+		tail = 0;		//队尾
+		size = 0;		//元素个数
 	}
 	
 	/**
 	 * @Description LoopQueue无参构造函数
 	 * @author Yin Guiqing
 	 * @date 2019年5月28日 下午9:07:29
+	 * @param int 容积
 	 */
 	public LoopQueue() {
 		this(10);
@@ -53,6 +54,7 @@ public class LoopQueue<T> implements Queue<T> {
 
 	@Override
 	public void enqueue(T e) {
+		
 		if ((tail + 1) % data.length == front) {
 			resize(getCapacity() * 2);
 		}
@@ -63,17 +65,31 @@ public class LoopQueue<T> implements Queue<T> {
 
 	@Override
 	public T dequeue() {
-		if (front == tail) {
+		
+		if (isEmpty()) {
 			throw new IllegalArgumentException("Cannot dequeue from an empty queue.");
 		}
 		
-		return null;
+		T result = data[front];
+		data[front] = null;
+		front = (front + 1) % data.length;
+		size --;
+		
+		if (size == getCapacity() / 4 && getCapacity() / 2 != 0) {
+			resize(getCapacity() / 2);
+		}
+		
+		return result;
 	}
 
 	@Override
 	public T getFront() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		if (isEmpty()) {
+			throw new IllegalArgumentException("The queue is empty.");
+		}
+		
+		return data[front];
 	}
 	
 	/**
@@ -85,6 +101,7 @@ public class LoopQueue<T> implements Queue<T> {
 	 * @throws
 	 */
 	private void resize(int newCapacity) {
+		
 		T[] newData = (T[]) new Object[newCapacity + 1];
 		for (int i = 0; i < size; i++) {
 			newData[i] = data[(i + front) % data.length];
@@ -92,6 +109,26 @@ public class LoopQueue<T> implements Queue<T> {
 		data = newData;
 		front = 0;
 		tail = size;
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		
+		StringBuilder result = new StringBuilder();
+		result.append(String.format("Queue: size = %d, capacity = %d. \n", size, getCapacity()));
+		result.append("front [");
+		for (int i = front; i != tail; i = (i+1) % data.length) {
+			result.append(data[i]);
+			if ((i+1) % data.length != tail) {
+				result.append(", ");
+			}
+		}
+		result.append("] tail");
+		
+		return result.toString();
 	}
 
 }
--- Queue/src/Main.java
+++ Queue/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * 
  */
@@ -19,16 +21,67 @@ public class Main {
 	 * @throws
 	 */
 	public static void main(String[] args) {
-		ArrayQueue<Integer> queue = new ArrayQueue<>();
-		for (int i = 0; i < 10; i++) {
-			queue.enqueue(i);
-			System.out.println(queue);
-			if (i % 3 == 2) {
-				queue.dequeue();
-				System.out.println(queue);
-			}
-		}
+		
+//		//ArrayQueue测试
+//		ArrayQueue<Integer> queue = new ArrayQueue<>();
+//		for (int i = 0; i < 10; i++) {
+//			queue.enqueue(i);
+//			System.out.println(queue);
+//			if (i % 3 == 2) {
+//				queue.dequeue();
+//				System.out.println(queue);
+//			}
+//		}
+		
+//		//LoopQueue测试
+//		LoopQueue<Integer> queue = new LoopQueue<>();
+//		for (int i = 0; i < 10; i++) {
+//			queue.enqueue(i);
+//			System.out.println(queue);
+//			if (i % 3 == 2) {
+//				queue.dequeue();
+//				System.out.println(queue);
+//			}
+//		}
+		
+		
+		//ArrayQueue与LoopQueue对比测试
+		int oprateCount = 100000;
+		
+		ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+		double arrayQueueTime = testQueue(arrayQueue, oprateCount);
+		System.out.println("ArrayQueue time: " + arrayQueueTime + " s.");
+		
+		LoopQueue<Integer> loopQueue = new LoopQueue<>();
+		double loopQueueTime = testQueue(loopQueue, oprateCount);
+		System.out.println("LoopQueue time: " + loopQueueTime + " s.");
 
+	}
+	
+	/**
+	 * @Description 测试使用queue运行oprateCount个enqueue和dequeue操作所需要的时间，单位秒
+	 * @author Yin Guiqing
+	 * @date 2019年5月30日 下午6:16:43
+	 * @param queue 队列
+	 * @param oprateCount 操作数
+	 * @return double 运行时间，秒
+	 * @throws
+	 */
+	private static double testQueue(Queue<Integer> queue, int oprateCount) {
+		
+		long startTime = System.nanoTime();
+		
+		Random random = new Random();
+		for (int i = 0; i < oprateCount; i++) {
+			queue.enqueue(random.nextInt(Integer.MAX_VALUE));
+		}
+		for (int i = 0; i < oprateCount; i++) {
+			queue.dequeue();
+		}
+		
+		long endTime = System.nanoTime();
+		
+		return (endTime - startTime) / 1000000000.0;
 	}
 
 }
