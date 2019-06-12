6dafa3f478c9813c35d2e30d52d2a8c09551112e
YinGuiqing <CPLASFyin@163.com>
2019-06-12 18:00:36 +0800

    2019-06-12 学完第6章前7节

---
+++ BST/src/Main.java
@@ -0,0 +1,33 @@
+/**
+ * @ClassName Main
+ * @Description 测试类
+ * @author Yin Guiqing
+ * @date 2019年6月7日 下午3:13:18
+ */
+public class Main {
+
+    /**
+     * @Description main方法
+     * @author Yin Guiqing
+     * @date 2019年6月7日 下午3:23:21
+     * @param args 
+     * @return void
+     * @throws
+     */
+    public static void main(String[] args) {
+        
+        BST<Integer> bst = new BST<>();
+        int nums[] = {5, 3, 6, 8, 4, 2};
+        for (int i : nums) {
+            bst.add(i);
+        }
+        bst.preOrder();
+        System.out.println();
+        
+        bst.inOrder();
+        System.out.println();
+        
+        bst.postOrder();
+        System.out.println();
+    }
+}
